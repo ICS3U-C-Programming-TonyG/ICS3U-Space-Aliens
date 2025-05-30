97a908d8123ae743e234c4916fe2f69b89778d0d
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-05-30 16:01:09 +0000

    hi

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Created By: Tony G
+
+# Date: 2025-05-29
+
+# Hello world - RST
+
+# Prints various things regarding  Tony G
+print("Hello, Tony G!")
+print("This particular fellow called Tony G also happens to like apple pie alot!")
